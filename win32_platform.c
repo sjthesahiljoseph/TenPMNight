b6837f8924efc30362ca86f03f0c5c671c90c654
sjthesahiljoseph <sjthesahiljoseph@gmail.com>
2026-03-12 01:51:41 +0500

    structure and typedefs and defines

--- win32_platform.c
+++ win32_platform.c
@@ -2,6 +2,16 @@
 
 #include <windows.h>
 
+typedef int b32;
+
+#define global_variable static
+#define internal static
+
+#define true 1
+#define false 0
+
+global_variable b32 running = true;
+
 LRESULT WindowCallback(HWND hWnd, UINT Msg, WPARAM wParam, LPARAM lParam)
 {
 	return DefWindowProc(hWnd, Msg, wParam, lParam);
@@ -26,6 +36,17 @@ int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int n
 	RegisterClassA(&windowClass);
 	
 	HWND window = CreateWindowExA(0, windowClass.lpszClassName, windowClass.lpszClassName, WS_VISIBLE|WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, 1280, 720, 0, 0, 0, 0);
+
+	while (running)
+	{
+
+		// Input
+
+		// Simulation
+
+		// Render
+		
+	}
 	
 }
 
