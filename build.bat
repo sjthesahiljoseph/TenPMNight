265b62f7c2e64c4bf3ac16df65d956532d3c6253
sjthesahiljoseph <sjthesahiljoseph@gmail.com>
2026-03-12 01:45:47 +0500

    we have a window

--- build.bat
+++ build.bat
@@ -7,7 +7,7 @@ if not exist build mkdir build
 
 pushd build
 
-cl ..\win32_platform.c user32.lib
+cl /nologo /Zi /FC ..\win32_platform.c user32.lib
 
 popd
 
--- win32_platform.c
+++ win32_platform.c
@@ -2,11 +2,31 @@
 
 #include <windows.h>
 
-int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
-			LPSTR lpCmdLine, int nShowCmd)
+LRESULT WindowCallback(HWND hWnd, UINT Msg, WPARAM wParam, LPARAM lParam)
+{
+	return DefWindowProc(hWnd, Msg, wParam, lParam);
+}
+
+int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
 {
 
-	return MessageBox(NULL, "TenPMNight", "TenPMNight", MB_ICONQUESTION);
+	WNDCLASSA windowClass = {0};
+
+	windowClass.style = CS_HREDRAW|CS_VREDRAW;
+	windowClass.lpfnWndProc = WindowCallback;
+	windowClass.cbClsExtra = 0;
+	windowClass.cbWndExtra = 0;
+	windowClass.hInstance = 0;
+	windowClass.hIcon = 0;
+	windowClass.hCursor = 0;
+	windowClass.hbrBackground = 0;
+	windowClass.lpszMenuName = 0;
+	windowClass.lpszClassName = "TenPMNight";
+	
+	RegisterClassA(&windowClass);
+	
+	HWND window = CreateWindowExA(0, windowClass.lpszClassName, windowClass.lpszClassName, WS_VISIBLE|WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, 1280, 720, 0, 0, 0, 0);
+	
 }
 
 
